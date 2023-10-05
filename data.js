bc7738e068030b2dd75e4be9856868c74e670071
Areeba Nafis <areebanafis065@gmail.com>
2023-10-06 04:20:55 +0530

    addedNotesApp&RC

---
+++ data.js
@@ -0,0 +1,213 @@
+export const data =[
+    {
+        id:"1",
+        thumbnail:"appe1.png",
+        name: "Vegetable Kawabs",
+        shName: "Kawab",
+        ingredients: ["2 medium Potatoes, boiled (approx. 1½ cups mashed potatoes)",
+        "1/2 cup Green Peas (boiled or blanched)",
+        "2½ tablespoons Gram Flour(Besan/Chickpea Flour)",
+        "1/4 cup finely chopped Onion",
+        "1 Green Chili, finely chopped, optional",
+        "1/2 cup finely chopped Cabbage",
+        "1/2 cup finely chopped Carrot",
+        "1/2 cup finely chopped Green Beans",
+        "1/4 cup finely chopped Capsicum",
+        "2 tablespoons finely chopped Coriander Leaves",
+        "1 tablespoon finely chopped Mint Leaves"],
+        instructions:["Add 2½ tablespoons gram flour(besan/chickpea flour) in a pan or a kadai and dry roast it over a low flame until nice aroma releases and it turns light golden. (No need to add the oil while roasting the flour.) Transfer the roasted flour to a bowl.",
+        "Wipe the pan with a cloth. Heat 1 tablespoon oil in the same pan over medium flame. Add 1/4 cup finely chopped onion and 1 finely chopped green chilli. Sauté until onion turns translucent or light pink.",
+        "Add 1/2 cup finely chopped cabbage, 1/2 cup finely chopped carrot, 1/2 cup finely chopped green beans, 1/4 cup finely chopped capsicum and salt.",
+        "Mix well and cook all the vegetables until soft but not mushy. (or Cook until most of the moisture is evaporated).",
+        "Let them cool to room temperature. Transfer them to the small jar of a mixer grinder or a chopper. Add 2 tablespoons finely chopped coriander leaves and 1 tablespoon finely chopped mint leaves.",
+        "Pulse them for a few times as you can see in the picture (don’t blend them very fine or don’t make a paste). Don’t add any water while blending the mixture.",
+        "Boil 2 medium potatoes and 1/2 cup green peas in salted water until soft. Transfer them to a colander to remove excess water.",
+        "Peel the potatoes and transfer to a large bowl. Either mash them or grate them. Add boiled green peas.",
+        "Add veggie mixture.",
+        " Add 1 teaspoon red chilli powder, 1 teaspoon cumin-coriander powder, 1/2 teaspoon garam masala powder, 1 teaspoon chaat masala powder, 1 tablespoon lemon juice, 2½ tablespoons roasted gram flour and salt to taste.",
+        " Mix well. Add 3 tablespoons breadcrumbs.",
+        " Mix well. If the mixture has too much moisture, add more breadcrumbs (as needed).",
+        " Grease your palms with oil. Take a ball size portion of the mixture and give it a cylindrical shape. Then take a wooden skewer/metal skewer and insert into the center.",
+        " Press the mixture evenly and give it a kabab shape/cylindrical shape. Place it in a plate.",
+        " Make the remaining kababs in a similar way.",
+        " Heat 1 tablespoon oil in a non-stick tava/pan or an iron tava. Place 2-3 kababs over hot tava. Roast/shallow fry them until golden brown from all the sides. Rotate them in between to roast them evenly from all the sides. If required, add more oil to roast the kababs.",
+        " Transfer them to a plate and serve with green coriander chutney or tomato ketchup and sliced onion and lemon wedges."]
+
+    },{
+        id:"2",
+        name: "Soya Bean Chat",
+        thumbnail:"appe2.png",
+        ingredients:["100g soya chunks",
+        "1/2 medium Red, Green, and Yellow peppers",
+        "1 small sliced onion",
+        "5 garlic cloves",
+        "Ginger juliennne",
+        "2 tbsp olive oil",
+        "1 tbsp lemon juice",
+        "2 tsp black pepper",
+        "Salt to taste",
+        "Chopped Coriander leaves"],
+        instructions:[
+            "Take soya chunks in a bowl and add 2-3 cups of hot water and set aside.",
+            "Heat-up the olive oil in a pan. Add green chili, garlic, and ginger and saute for around 30 seconds.",
+            "Add onion, saute for a minute and add red, green, and yellow peppers and saute for around 2 minutes.",
+            "Meanwhile, drain the water and rinse the chunks with fresh water, and squeeze out the excess water from soya chunks.",
+            "Now add red chili powder(optional), black pepper, and salt. Mix well and add soya chunks.",
+            "Add and mix lemon juice and chopped coriander leaves. Serve hot and enjoy with your friends and family. Thanks!"]
+    },
+    {
+        id:"3",
+        name: "Air Fried Paneer",
+        thumbnail:"appe3.jpg",
+        ingredients:[
+            "400g paneer, cut into cubes",
+            "1 cup yogurt",
+            "1 tsp ginger paste",
+            "1 tsp garlic paste",
+            "1 tsp cumin powder",
+            "1 tsp coriander powder",
+            "1 tsp garam masala",
+            "1 tsp red chili powder",
+            "2 tbsp oil",
+            "Salt to taste",
+            "Lemon juice for garnish",
+            "Onion and tomato wedges for serving"
+        ],
+        instructions:[
+            "In a bowl, mix yogurt, ginger paste, garlic paste, cumin powder, coriander powder, garam masala, red chili powder, oil, and salt.",
+            "Add paneer cubes to the bowl and mix well to coat each piece with the marinade.",
+            "Preheat the air fryer at 200°C for 5 minutes.",
+            "Arrange the marinated paneer cubes on the air fryer basket in a single layer.",
+            "Air fry for 10 minutes, then flip the paneer cubes and air fry for another 5 minutes until golden brown.",
+            "Garnish with lemon juice and serve hot with onion and tomato wedges."
+        ]
+    },
+    {
+        id:"4",
+        name: "Detox Water",
+        thumbnail:"drinks1.jpg",
+        ingredients:["1 Cucumber",
+        "8-10 Mint leaves",
+        "2 Tbsp Lemon juice",
+        "Ice cubes/Iced Water",
+        "Lemon rings",
+        "Mint leaves",
+        "Ginger Slices"],
+        instructions:["Thoroughly Rinse and Chop cucumber",
+        "Add Lemon Slices, mint leaves , chopped cucumber and ginger slices to 1 litre water in glass jar",
+        "Leave it overnight",
+        "Add lemon juice, black salt and dilute with water if needed.",
+        "Pour the beverages into glasses, put some ice cubes and garnish with lemon rings and mint leaves."
+        ]
+    },
+    {
+        id:"5",
+        name: "Chia Seed Drink",
+        thumbnail:"drinks2.jpg",
+        ingredients:["2 Spoon Chia Seed",
+        "1 Tbsp Apple Cider Vineger(ACV)",
+        "Black salt to taste",
+        "2 Tbsp Lemon Juice"],
+        instructions:[
+            "Add Lemon Slices, chia seeds and ACV to a glass of water",
+            "Leave it overnight",
+            "Add black salt and dilute with water if needed.",
+            "Pour the beverages into glasses, put some ice cubes and garnish with mint leaves."
+        ]
+    },
+    {
+        id:"6",
+        name: "Chocolate Smoothie",
+        thumbnail:"drinks3.jpg",
+        ingredients:[
+            "1 cup/150g frozen banana slices or 1 large ripe fresh banana",
+            "1 tbsp almond butter or peanut butter",
+            "1/2 cup oats",
+            "1 tbsp of cacao powder",
+            "1 tsp of chia seeds",
+            "1 cup/250ml of dairy free milk unsweetened - I used almond",
+            "1/2 Tbsp vanilla essence",
+            "Dark Chocolate crumbs"
+        ],
+        instructions:[
+            "Freeze Bananas a day before If you don't have frozen bananas, you can use a large ripe fresh banana, but the smoothie won't come out as thick.",
+            "Dry Roast rolled oats or use Instant oats",
+            "Add all the ingredients to a high-speed blender and blitz until smooth.",
+            "Grate Dark Chocolate",
+            "Garnish with chopped Dark Chocolate crumbs",
+            "Serve and enjoy!"
+        ]
+    },
+    {
+        id:"7",
+        name: "Sweet Corns",
+        thumbnail:"snack1.jpg",
+        ingredients:[
+            "Corn: You can either extract kernels from a corn cob or get ready-to-use sweet corn kernels.",
+            "For Seasoning Water: Garlic, Ginger, Bay Leaf, Black Peppercorns, Salt",
+            "Vegetables: Onion, Tomato",
+            "Herbs: Fresh Coriander Leaves, Spring Onion (Green Onion)",
+            "Spices: Chaat Masala, Cumin Powder, Red Chili Powder, Rock Salt, Black Pepper Powder",
+            "Other Ingredients: Green Chili, Lemon Juice"
+        ],
+        instructions:[
+            "Boil water in a saucepan along with ginger, garlic, black peppercorns, bay leaf, and salt.",
+            "Once the water starts boiling add corn kernels.",
+            "Boil them for 3 – 5 minutes or till they are tender and cooked.",
+            "Transfer corn kernels to a colander. Drain all the water. Add butter to the warm corn and mix nicely. Set it aside.",
+            "Combine sweet corn kernels, chopped ingredients, spices, salt, lemon juice, and fresh coriander.",
+            "Mix and the corn chaat is ready to serve!"
+        ]
+    },
+    {
+        id:"8",
+        name: "Roasted Fox-nuts",
+        thumbnail:"snack2.jpg",
+        ingredients:[
+            "3 cup foxnuts (makhana or phool makhana)",
+            "1/4 teaspoon turmeric powder",
+            "1/2 teaspoon red chili powder or cayenne pepper, add as required",
+            "1 teaspoon chaat masala powder or add as required",
+            "Rock salt (edible and food grade), add as required",
+            "2 to 3 teaspoons oil or ghee"
+        ],
+        instructions:[
+            "Heat oil in a heavy wok or kadai.",
+            "Add the makhana and roast for a good 10 to 12 minutes on low heat, till they become crisp. Keep on stirring in between.",
+            "Next, add all the spice powders and rock salt, except chaat masala.",
+            "Turn off the heat as you don't want the spice powders to get burnt.",
+            "Stir the whole mixture well.",
+            "Lastly, sprinkle the chaat masala powder and again mix well.",
+            "Serve Roasted Makhana when they become lukewarm or cool at room temperature.",
+            "Remember that once they cool down, store in an air-tight container."
+    ]
+    },
+    {
+        id:"9",
+        name: "Chocolate Cookies",
+        thumbnail:"snack3.jpg",
+        ingredients:[
+            "Spelt flour or All-Purpose (1 to 1) Gluten Free Flour",
+            "Raw honey",
+            "Raw Cocao Powder (or regular Cocoa Powder)",
+            "Dark chocolate chips (70% or darker, naturally sweetened if possible)",
+            "Plant-Based butter (such as Earth Balance) or Coconut oil",
+            "Eggs",
+            "Vanilla Extract",
+            "Non-dairy milk of choice",
+            "Salt, baking powder, and baking soda"
+        ],
+        instructions:[
+            "Preheat oven to 350 degrees.",
+            "In a bowl whisk eggs, milk, and vanilla.",
+            "In a small saucepan melt butter (or coconut oil) with the dark chocolate chips and the honey.",
+            "Add the melted chocolate mixture to the egg mixture and whisk until combined.",
+            "Add in dry ingredients (flour, cocao powder baking powder and soda, and salt), and stir until a smooth cookie batter is achieved.",
+            "Cover a baking pan with parchment paper.",
+            "Scoop tablespoon sized portions of the batter into balls and press down lightly to flatten (makes 16 small cookies).",
+            "Bake cookies for 8 minutes until just puffy.",
+            "Let cool completely then drizzle with the melted chocolate and sprinkle with cocao nibs if you prefer (the chocolate drizzle will harden in the fridge for a few minutes).",
+            " Enjoy!"
+        ]
+    },
+]
--- script.js
+++ script.js
@@ -1,35 +1,41 @@
-const contactMe =document.querySelector(".contactMe");
-const infoBox = document.querySelector(".infoBox");
-const infoBtn = document.querySelector(".infoBtn")
+// Contact Me Modal
+const contactMe = document.querySelector(".contactMe");
+function myFunc2(){
+    contactMe.classList.toggle('hidden');
+ }
+
+//Dark Mode
+function myFunction(){
+    var element =document.body;
+    element.dataset.bsTheme = element.dataset.bsTheme == "light" ? "dark" : "light";
+ }
+ 
+
+//Timer Logic 
 const playBtn = document.querySelector(".playBtn")
 const timeBox = document.querySelector(".timeBox");
 const timer = document.querySelector(".timer")
 
-let seconds = 0;
-let minutes =0;
-let hours = 0;
+let seconds = localStorage.getItem("sec")||0;
+let minutes = localStorage.getItem("min")||0;
+let hours =  localStorage.getItem("h")|| 0;
 let timerId;
 
-function myFunction(){
-   var element =document.body;
-   element.dataset.bsTheme = element.dataset.bsTheme == "light" ? "dark" : "light";
-}
+timer.innerHTML =`${hours<10?`0${hours}`:hours}:${minutes<10?`0${minutes}`:minutes}:${seconds<10?`0${seconds}`:seconds}`
 
-function myFunc2(x){
-   x.classList.toggle('hidden');
-}
 // infoBtn.addEventListener('mouseover',()=>{
 //     infoBox.classList.toggle('hidden');
 // })
 const reset = document.createElement("span");
-reset.innerHTML = '<button name = "reset"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg></button>';
+reset.innerHTML = '<button id="reset" name = "reset"><svg id="reset" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16"><path id="reset" d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path id="reset" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg></button>';
 
 
 function handleClick (event){
-    console.log(event.target.parentElement.name);
-    if(event.target.parentElement.name === "play"){
-        playBtn.innerHTML ='<button name="stop"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-stop" viewBox="0 0 16 16"><path d="M3.5 5A1.5 1.5 0 0 1 5 3.5h6A1.5 1.5 0 0 1 12.5 5v6a1.5 1.5 0 0 1-1.5 1.5H5A1.5 1.5 0 0 1 3.5 11V5zM5 4.5a.5.5 0 0 0-.5.5v6a.5.5 0 0 0 .5.5h6a.5.5 0 0 0 .5-.5V5a.5.5 0 0 0-.5-.5H5z"/></svg></button>';
+    console.log(event.target.id);
+    if(event.target.id === "play"){
         timeBox.appendChild(reset);
+        playBtn.innerHTML ='<button name="stop" id="stop" ><svg id="stop" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-stop" viewBox="0 0 16 16"><path id="stop" d="M3.5 5A1.5 1.5 0 0 1 5 3.5h6A1.5 1.5 0 0 1 12.5 5v6a1.5 1.5 0 0 1-1.5 1.5H5A1.5 1.5 0 0 1 3.5 11V5zM5 4.5a.5.5 0 0 0-.5.5v6a.5.5 0 0 0 .5.5h6a.5.5 0 0 0 .5-.5V5a.5.5 0 0 0-.5-.5H5z"/></svg></button>';
+        
         timerId=setInterval(()=>{
             seconds++;
             if (seconds>59){
@@ -43,18 +49,199 @@ function handleClick (event){
             timer.innerHTML = `${hours<10?`0${hours}`:hours}:${minutes<10?`0${minutes}`:minutes}:${seconds<10?`0${seconds}`:seconds}`;
         },1000);
     }
-    if(event.target.parentElement.name === "stop"){
+    if(event.target.id === "stop"){
         clearInterval(timerId);
+        localStorage.setItem("sec",seconds);
+        localStorage.setItem("min",minutes);
+        localStorage.setItem("h",hours);
     }
-    if(event.target.parentElement.name === "reset"){
+    if(event.target.id === "reset"){
         clearInterval(timerId);
         seconds = hours =minutes =0;
         timer.innerHTML = `${hours<10?`0${hours}`:hours}:${minutes<10?`0${minutes}`:minutes}:${seconds<10?`0${seconds}`:seconds}`;
         timeBox.removeChild(reset);
-        playBtn.innerHTML =' <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play" viewBox="0 0 16 16"><path d="M10.804 8 5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z"/></svg>'
+        playBtn.innerHTML =' <svg  id="play" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play" viewBox="0 0 16 16"><path  id="play" d="M10.804 8 5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z"/></svg>'
+        localStorage.setItem("sec",seconds);
+        localStorage.setItem("min",minutes);
+        localStorage.setItem("h",hours);
     }
-    
-
 
 }
 timeBox.addEventListener('click', handleClick);
+
+// Notes App Logic
+function uuid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0,
+            v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+const titleI = document.querySelector(".titleI");
+const inputI = document.querySelector(".noteI");
+const addBtn = document.querySelector(".add-btn");
+let showPinned =document.querySelector(".pinned");
+let showOthers = document.querySelector(".others");
+let notesDisplay = document.querySelector(".notesDisplay");
+
+
+// let localData = JSON.parse(localStorage.getItem("todos"));
+let notesList = JSON.parse(localStorage.getItem("todos")) || [];
+
+addBtn.addEventListener('click',()=>{
+    if(titleI.value.trim().length>0 || inputI.value.trim().length>0){
+        notesList.push({id: uuid(), note: inputI.value,title:titleI.value, isPinned: false});
+    }
+    titleI.value="";
+    inputI.value ="";
+    localStorage.setItem("todos",JSON.stringify(notesList));
+    showOthers.innerHTML =renderNotes(notesList);
+})
+
+const renderNotes=(List)=>{
+    let newNote = List.map(({
+        id,
+        note,
+        title,
+        isPinned
+    }) => 
+    {
+    return(
+    `<li class="note"> 
+        <div class="flexBtwn">
+            <span class="notetitle">${(title.slice(0,1).toUpperCase() + title.slice(1,title.length))}
+            </span>
+            <div class="btnDiv" data-id="${id}">
+                <span class='pin-btn ${isPinned ? "material-icons" : "material-icons-outlined" }' data-type =pin data-pinned=${isPinned} data-id="${id}"> push_pin </span>
+                <span class="material-icons-outlined del-btn" data-id="${id}" data-type="delete">delete</span>
+            </div>
+        </div>
+        <hr class="seperator">
+        <div class="flex" data-id="${id}">${note}</div>
+   </li>`)
+   });
+   newNote = newNote.join("");
+    return newNote;
+}
+
+
+notesDisplay.addEventListener('click',(event)=>{
+    // console.log();
+    const clickedNoteId=event.target.dataset.id;
+    const clickedNoteType= event.target.dataset.type;
+    switch (clickedNoteType){
+        case "delete":
+            notesList = notesList.filter(({id})=> id.toString()!==clickedNoteId);
+            showOthers.innerHTML = renderNotes(notesList.filter(({isPinned})=>!isPinned));
+            showPinned.innerHTML = renderNotes(notesList.filter(({isPinned})=>isPinned));
+            localStorage.setItem("todos",JSON.stringify(notesList));
+            break;
+        case "pin":
+            notesList =notesList.map((note)=> note.id.toString()===clickedNoteId? {...note,isPinned:!note.isPinned}:note);
+            showOthers.innerHTML = renderNotes(notesList.filter(({isPinned})=>!isPinned));
+            showPinned.innerHTML = renderNotes(notesList.filter(({isPinned})=>isPinned));
+            localStorage.setItem("todos",JSON.stringify(notesList));
+            break;
+        default: 
+        console.log("");
+    }
+});
+
+showOthers.innerHTML =renderNotes(notesList);
+
+
+// recipe card
+
+import {data} from "./data.js";
+
+const rCard = document.querySelector(".recipeCard");
+const rTi = document.querySelector(".rTi");
+const rImg = document.querySelector(".rImg");
+
+const prevRecBtn = document.querySelector(".prevRecBtn");
+const nextRecBtn = document.querySelector(".nextRecBtn");
+const currPage = document.querySelector(".currPage");
+
+const currRecipeId= localStorage.getItem("recipeId");
+
+const currRecipeObject = data.find((item)=>item.id===currRecipeId);
+
+
+const renderRecipe = (currRecipeObject)=>{
+    const imgSrc = `./assets/${currRecipeObject.thumbnail}`;
+    return(
+        `<div class="recipeHead col-12 d-flex align-items-center justify-content-center"><h3 class="rTi">${currRecipeObject.name}</h3></div>
+
+    <div class="imgctn col-xs-12 col-md-5 col-xxl-5">
+        <img class="img-fluid rImg" src="${imgSrc}" alt="Kawab Picture">
+    </div>
+    <div class="ingredients col-xs-12 col-md-6  offset-1 col-xxl-4 ">
+        <h4>Ingredients</h4>
+        <ul class=" ingredientsList"></ul>
+    </div>
+    <div class=" instructions instructionsList col-12 ">
+        <h4>Instructions</h4>
+        <ul class=" instructionsList"></ul>
+    </div>
+    <div class="recipeFooter col-12">
+        <nav>
+            <ul class="pagination justify-content-center">
+
+                <li id="preRecBtn" class="pageitem"><button id="preRecBtn" class="disabled1"><span id="preRecBtn" class = "material-icons-outlined">arrow_back_ios</span>
+                </button></li>
+
+                <li id="x" class="pageitem my-auto cPC"><span id="x" class="currPage">${currRecipeObject.name}</span></li>
+
+                <li class="pageitem" id ="nextRecBtn"><button  id ="nextRecBtn" class="disabled2" >
+                    <span id ="nextRecBtn" class="material-icons-outlined"> arrow_forward_ios</span>
+                </button></li>
+            </ul>
+        </nav>
+    </div>`
+    );
+}
+
+
+const renderInstructionList = ((List)=>{
+    List.map((item,index)=>{
+        const spanItem = document.createElement("span");
+        spanItem.innerHTML=`STEP-${index+1}:`;
+        const listItem = document.createElement("li");
+        listItem.appendChild(spanItem);
+        listItem.appendChild(document.createTextNode(" " + item));
+        document.querySelector(".instructionsList").appendChild(listItem);
+    });
+})
+const renderIngredientsList = ((List)=>{
+    List.map((item)=>{
+        const listItem = document.createElement("li");
+        listItem.appendChild(document.createTextNode(item));
+        document.querySelector(".ingredientsList").appendChild(listItem);
+    });
+})
+
+
+rCard.innerHTML =renderRecipe(currRecipeObject);
+renderInstructionList(currRecipeObject.instructions);
+renderIngredientsList(currRecipeObject.ingredients);
+
+if(currRecipeId==="1"||currRecipeId==="4"||currRecipeId==="7"){
+   document.querySelector(".disabled1").setAttribute("disabled",'');
+}
+if(currRecipeId==="3"||currRecipeId==="6"||currRecipeId==="9"){
+    document.querySelector(".disabled2").setAttribute("disabled",'');
+ }
+
+document.querySelector(".pagination").addEventListener('click',(e)=>{
+ console.log(e.target.id);
+ if(e.target.id==="nextRecBtn"){
+    localStorage.setItem("recipeId",`${Number(currRecipeId)+1}`);
+    window.location.reload("true");
+ }
+ if(e.target.id==="preRecBtn"){
+    localStorage.setItem("recipeId",`${Number(currRecipeId)-1}`);
+    window.location.reload("true");
+ }
+});
+
